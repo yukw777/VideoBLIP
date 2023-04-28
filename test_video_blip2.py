d545c99b54ffbf48e66d3d8c50c43249e77d2958
Peter Yu <2057325+yukw777@users.noreply.github.com>
2023-04-28 14:41:15 -0400

    Implement VideoBlip2VisionModel
    This is a simple augmentation of Blip2VisionModel that can handle videos,
    or a series of frames/images.

---
+++ test_video_blip2.py
@@ -0,0 +1,79 @@
+import pytest
+import torch
+from transformers.models.blip_2.configuration_blip_2 import Blip2VisionConfig
+
+from video_blip2 import VideoBlip2VisionModel
+
+
+@pytest.mark.parametrize("output_hidden_states", [True, False])
+@pytest.mark.parametrize("output_attentions", [True, False])
+@pytest.mark.parametrize("time", [1, 8])
+@pytest.mark.parametrize("batch", [1, 4])
+@pytest.mark.parametrize(
+    "config",
+    [
+        Blip2VisionConfig(
+            hidden_size=8,
+            intermediate_size=16,
+            projection_dim=4,
+            num_hidden_layers=2,
+            num_attention_heads=4,
+            patch_size=8,
+        ),
+        Blip2VisionConfig(
+            hidden_size=16,
+            intermediate_size=32,
+            projection_dim=8,
+            num_hidden_layers=4,
+            num_attention_heads=8,
+            patch_size=12,
+        ),
+    ],
+)
+def test_video_blip2_vision_model_forward(
+    config: Blip2VisionConfig,
+    batch: int,
+    time: int,
+    output_attentions: bool,
+    output_hidden_states: bool,
+) -> None:
+    model = VideoBlip2VisionModel(config)
+    outputs = model(
+        pixel_values=torch.rand(
+            # channel is pretty much always 3
+            batch,
+            3,
+            time,
+            config.image_size,
+            config.image_size,
+        ),
+        output_attentions=output_attentions,
+        output_hidden_states=output_hidden_states,
+    )
+    last_hidden_state, pooler_output, hidden_states, attentions = outputs
+    # divide the image into non-overlapping patches, flatten them out,
+    # then add a cls token.
+    num_tokens = (config.image_size // config.patch_size) ** 2 + 1
+    assert last_hidden_state.size() == (batch, time, num_tokens, config.hidden_size)
+    assert pooler_output.size() == (batch, time, config.hidden_size)
+
+    if output_attentions:
+        assert len(attentions) == config.num_hidden_layers
+        for attn in attentions:
+            assert attn.size() == (
+                batch,
+                time,
+                config.num_attention_heads,
+                num_tokens,
+                num_tokens,
+            )
+    else:
+        assert attentions is None
+
+    if output_hidden_states:
+        # num_hidden_layers + 1 for embeddings
+        assert len(hidden_states) == config.num_hidden_layers + 1
+        for hidden in hidden_states:
+            assert hidden.size() == (batch, time, num_tokens, config.hidden_size)
+    else:
+        assert hidden_states is None
---
+++ video_blip2.py
@@ -0,0 +1,84 @@
+import torch
+from transformers import Blip2VisionModel
+from transformers.modeling_outputs import BaseModelOutputWithPooling
+
+
+class VideoBlip2VisionModel(Blip2VisionModel):
+    """A simple, augmented version of Blip2VisionModel to handle videos."""
+
+    def forward(
+        self,
+        pixel_values: torch.FloatTensor | None = None,
+        output_attentions: bool | None = None,
+        output_hidden_states: bool | None = None,
+        return_dict: bool | None = None,
+    ) -> tuple | BaseModelOutputWithPooling:
+        """Flatten `pixel_values` along the batch and time dimension, pass it
+        through the original vision model, then unflatten it back.
+
+        :param pixel_values: a tensor of shape (batch, channel, time, height, width)
+
+        :returns:
+            last_hidden_state: a tensor of shape (batch, time, seq_len, hidden_size)
+            pooler_output: a tensor of shape (batch, time, hidden_size)
+            hidden_states:
+                a tuple of tensors of shape (batch, time, seq_len, hidden_size), one for
+                the output of the embeddings + one for each layer
+            attentions:
+                a tuple of tensors of shape (batch, time, num_heads, seq_len, seq_len),
+                one for each layer
+        """
+        if pixel_values is None:
+            raise ValueError("You have to specify pixel_values")
+
+        batch, _, time, _, _ = pixel_values.size()
+
+        # flatten along the batch and time dimension to create a tensor of shape
+        # (batch * time, channel, height, width)
+        flat_pixel_values = pixel_values.permute(0, 2, 1, 3, 4).flatten(end_dim=1)
+
+        vision_outputs: BaseModelOutputWithPooling = super().forward(
+            pixel_values=flat_pixel_values,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=True,
+        )
+
+        # now restore the original dimensions
+        # vision_outputs.last_hidden_state is of shape
+        # (batch * time, seq_len, hidden_size)
+        seq_len = vision_outputs.last_hidden_state.size(1)
+        last_hidden_state = vision_outputs.last_hidden_state.view(
+            batch, time, seq_len, -1
+        )
+        # vision_outputs.pooler_output is of shape
+        # (batch * time, hidden_size)
+        pooler_output = vision_outputs.pooler_output.view(batch, time, -1)
+        # hidden_states is a tuple of tensors of shape
+        # (batch * time, seq_len, hidden_size)
+        hidden_states = (
+            tuple(
+                hidden.view(batch, time, seq_len, -1)
+                for hidden in vision_outputs.hidden_states
+            )
+            if vision_outputs.hidden_states is not None
+            else None
+        )
+        # attentions is a tuple of tensors of shape
+        # (batch * time, num_heads, seq_len, seq_len)
+        attentions = (
+            tuple(
+                hidden.view(batch, time, -1, seq_len, seq_len)
+                for hidden in vision_outputs.attentions
+            )
+            if vision_outputs.attentions is not None
+            else None
+        )
+        if return_dict:
+            return BaseModelOutputWithPooling(
+                last_hidden_state=last_hidden_state,
+                pooler_output=pooler_output,
+                hidden_states=hidden_states,
+                attentions=attentions,
+            )
+        return (last_hidden_state, pooler_output, hidden_states, attentions)
